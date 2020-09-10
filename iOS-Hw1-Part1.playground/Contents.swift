7cf40f599f65fc5ab9e158c92eb9d0aba85e6244
Talal-Alkhamis-C77 <70804941+Talal-Alkhamis-C77@users.noreply.github.com>
2020-09-10 21:53:18 +0300

    Part 1 Done

--- iOS-Hw1-Part1.playground/Contents.swift
+++ iOS-Hw1-Part1.playground/Contents.swift
@@ -5,14 +5,17 @@
  
  */
 
-let name = "Ahmad"
+
+ var
+name = "Ahmad"
 
 name = "Salem"
 
 
 var myWeight = 60
-var FriendWeight = 80.5
-var height = 1.5
+var FriendWeight = 80
+var height = 160
+
 
 
 var weightSum = myWeight + FriendWeight
@@ -38,32 +41,37 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
+var firstName1 = "talal"
+var lastName1 = "alkhamis"
+var email1 = "talal@gmail.com"
+var phoneNumber1 = "12345678"
+var age1 = 15
+var country1 = "Indai"
+var area1 = "Mumbai"
+var password = "P@ssw0rd"
 var isKuwaiti = false
 
 //Person 2
-
-
-
-
-
-
-
-
-
+var firstName2 = "ali"
+var lastName2 = "alibrahim"
+var email2 = "ali@gmail.com"
+var phoneNumber2 = "87654321"
+var age2 = 18
+var country2 = "USA"
+var area2 = "NEW YORK"
+var password2 = "1234567"
+var isKuwaiti2 = false
 
 //Person 3
-
-
-
-
+var firstName3 = "Khalid"
+var lastName3 = "Alshmri"
+var email3 = "khalid@gmail.com"
+var phoneNumber3 = "9876010"
+var age3  = 20
+var country3 = "kuwait"
+var area3  = "Kafain"
+var password3 = "4567"
+var isKuwaiti3 = false
 
 
 
@@ -72,7 +80,7 @@ var isKuwaiti = false
 /*
 - - - -
 # المطلوب الثالث
-* قم بمقارنة معلومات كل شخص مع الشخص الاخر
+* ق م بمقارنة معلومات كل شخص مع الشخص الاخر
  * قارن الاعمار اذا كانت متساوية ام لا
  * قارن منطقة السكن اذا كانت متساوية ام لا
  * قارن اذا عمر الاشخاص اكبر من ١٨
@@ -81,4 +89,39 @@ var isKuwaiti = false
  * قارن اذا عمر الاشخاص لا يساوي  ١٧
 
  
- */
+ 
+ 
+
+ 
+ 
+ 
+ */age1 == age2 || age2 == age3 || age1 == age3
+country1 == country2 || country2 == country3 || country1 == country3
+age1 < age2 && age2 < age3 && age1 < age3
+age1 > 18 || age2 > 18 || age3 > 18
+age1 < 18 || age2 < 18 || age3 < 18
+age1 == 17 || age2 == 17 || age3 == 17
+age1 != 17 || age2 != 17 || age3 != 17
+
+
+
+
+
+
+print (" * *" )
+print("*   * ")
+print(" * *           ******            * *")
+print("              *       *         *     *")
+print("  *           *       *          *")
+print("  *           *       *           *")
+print("  *           *       *             *")
+print("  *           *       *              *")
+print("  *           *       *               *")
+print("  *           *       *                *")
+print("  *           *       *         *     *")
+print("  *           *       *          *   *")
+print("  *            ******              *")
+
+
+
+
